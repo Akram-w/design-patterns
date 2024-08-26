9e17acd45858e0805ad09a0667de829af9117a43
Akram-w <akramwaseem792@gmail.com>
2024-08-26 22:34:30 +0530

    refactor

--- src/factoryPattern/Main.java
+++ src/factoryPattern/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public  static void main(String[] args){
         // USE THIS PATTER FOR DB CONNECTION OR LOGGER OR SIMILAR SCENARIOS
         GetPlanFactory getPlanFactory = new GetPlanFactory();
+        
         // DOMESTIC bill example
         Plan domesticPlan = getPlanFactory.getPlan(PlanTypes.DOMESTIC);
 
